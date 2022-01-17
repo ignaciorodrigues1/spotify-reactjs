f7b863ec36669a22351fc6ed8f387ef461341ed1
Ignacio Rodrigues <ignacio.rodrigues@blackid.com.ar>
2022-01-16 22:04:16 -0300

    feat: update app spotify

--- spotify/components/Sidebar.js
+++ spotify/components/Sidebar.js
@@ -32,7 +32,7 @@ function Sidebar() {
   console.log(playlists);
   
   return (
-    <div className="text-gray-500 p-5 text-xs border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen lg:text-sm sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
+    <div className="text-gray-500 p-5 text-xs border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen lg:text-sm sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex pb-36">
       <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
